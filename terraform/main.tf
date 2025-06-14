a55d7911a8915e3e1127ce052d297036e0eb3258
d3n3b3 <drumatyrg@gmail.com>
2025-06-14 19:57:24 +0000

    v8

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-#resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = var.public_key
-#}
-
-
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow SSH and HTTP"
