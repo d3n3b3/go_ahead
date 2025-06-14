fa6fffd8c8b9fb81bd06f013019f45f985c9d9db
d3n3b3 <drumatyrg@gmail.com>
2025-06-14 19:52:30 +0000

    v7

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,10 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_key_pair" "deployer" {
+#resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
-}
+#}
 
 
 resource "aws_security_group" "web_sg" {
@@ -44,7 +44,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
+  key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
